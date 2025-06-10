2081e20b54752077ac05dd98a13ff229251d401f
sabenitez-unal <sabenitez@unal.edu.co>
2025-06-10 15:01:23 -0500

    Integración de las dependencias de la aplicación: Astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   
   vite: {
     plugins: [tailwindcss()]
