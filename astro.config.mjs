beda792c8d97b6218cb146aaea7df2b4a1ef6798
gsvelezg <gvelezg@unal.edu.co>
2025-06-11 10:34:57 -0500

    Ajustes de dependencias: la dependencia astrojs/tailwindcss se elimina - deprecated. Se usa sólo tailwindcss/vite en su lugar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   
-  integrations: [tailwind(), icon()],
+  integrations: [icon()],
   
   vite: {
     plugins: [tailwindcss()]
